19491f1a29bced4733c461989b835c4521c513ed
Harsh Verma <harshvermadev@gmail.com>
2026-05-05 16:24:36 +0530

    Form Handling.

---
+++ formhandling.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <h1> "WELCOME to PHP" </h1>
+</head>
+<body>
+
+    <form method="POST">
+        Name:
+        <input type="text" name="name" required>
+        <br><br>
+        Email:
+        <input type="text" name="Email" required>
+        <br><br>
+        Number:
+        <input type="text" name="number" required>
+        <br><br>
+        <input type="submit" value="submit">
+
+</body>
+</html>
