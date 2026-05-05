b669f705818b6e2d13791ffdfa7cfcefabbca7a3
Harsh Verma <harshvermadev@gmail.com>
2026-05-05 16:52:57 +0530

    GET and REQUEST

---
+++ get_post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>  
+    <h1> "WELCOME to PHP" </h1>
+</head>
+<body>
+    <?php
+    echo $_GET["name"];
+    echo $_REQUEST["Email"];
+
+    ?>
+    </body>
+
+    </html>
